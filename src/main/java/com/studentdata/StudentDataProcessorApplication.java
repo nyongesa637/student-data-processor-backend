71f1ca11b94217040cd5f93357dac22e48419a45
nyongesa637 <nimrodnyongesa7@gmail.com>
2026-02-08 23:01:21 +0300

    Add Spring Boot application entry point

---
+++ src/main/java/com/studentdata/StudentDataProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.studentdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDataProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentDataProcessorApplication.class, args);
+    }
+}
